3cb640c900a9356c1f3a20c341a8ad18653abab1
Charles Goddard <Charles.Goddard@students.olin.edu>
2013-04-11 04:13:52 -0400

    Removed duplicate definition of x2 in test.lisp

--- test.lisp
+++ test.lisp
@@ -1,12 +1,6 @@
 (define area (lambda (r) (* 3.141592653 (* r r))))
 (display "The area of a circle with radius 3 is" (area 3) ".")
 
-(define x2
-	(lambda (n)
-		(+ n n)))
-
-(display (equal 18 (x2 9)))
-
 (defmacro defun (name args exp)
 	(define name (lambda args exp)))
 
@@ -14,3 +8,4 @@
 	(+ n n))
 
 (display (x2 3))
+(display (equal 18 (x2 9)))
